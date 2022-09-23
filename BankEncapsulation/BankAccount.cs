748e687ced1b7ce8fd08db62acca57eb4a2b95ef
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-23 14:20:50 -0500

    First commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public double Deposit(double userDepositAnswer)
+        {
+            
+            return balance += userDepositAnswer;
+        }
+
+        public double Withdraw(double userWithdrawAnswer)
+        {
+            return balance -= userWithdrawAnswer;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using BankEncapsulation;
+
+
+
+
+BankAccount NewAccount = new BankAccount();
+
+
+
+Console.WriteLine("Type the number you want to deposit: ");
+
+double amountDeposited = Convert.ToDouble(Console.ReadLine());
+
+NewAccount.Deposit(amountDeposited);
+
+Console.WriteLine();
+
+
+Console.WriteLine("Type the number you want to withdraw: ");
+double amountWithdrawn = Convert.ToDouble(Console.ReadLine());
+NewAccount.Withdraw(amountWithdrawn);
+
+
+
+Console.WriteLine($"Your balance: {NewAccount.GetBalance()}");
+
